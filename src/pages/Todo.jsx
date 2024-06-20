cbca5ff495f81cb068e16976af834292e1504c63
Nikita <nik.poteschnow2017@yandex.ru>
2024-06-20 11:50:57 +0300

    day7

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -3,8 +3,8 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
-import TodoForm from '../components/TodoForm';
-import '../styles/Todo.css';
+import TodoForm from '../components/TodoForm/TodoForm';
+import '../pages/Todo.css';
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
