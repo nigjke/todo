92f5a65f730d2efb8808b53d1c1906a8c9f6ca98
Nikita <nik.poteschnow2017@yandex.ru>
2024-06-28 11:57:49 +0300

    feat: add Dark theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import './App.css';
-
-import Todo from './pages/Todo';
+import * as React from 'react';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Todo from '../src/pages/Todo';
 
 export default function App() {
-    return <Todo />;
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+            }),
+        [prefersDarkMode],
+    );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Todo />
+        </ThemeProvider>
+    );
 }
